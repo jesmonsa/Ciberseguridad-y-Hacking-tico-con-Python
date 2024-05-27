481dcbfc154b88d51173957a867c88ec0b59b9f7
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-27 18:58:44 -0400

    Ultimo commit

--- 02-input-print.py
+++ 02-input-print.py
@@ -2,5 +2,5 @@ print ("Hola, bienvenido a Python")
 
 print ("Hola, bienvenido a Python", "Clase 1")
 
-print ("Hola, bienvenido a Python", "Clase 1", "Curso de Python", sep=" - ")
+print ("Hola, bienvenido a Python", "Clase 1", "Curso de Python", sep=" - ", end=" \n\n")
 print ("Fundamentos e Introducción a Python", end=" - ")
