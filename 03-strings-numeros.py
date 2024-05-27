3cd38e37f5b85c58286a27b322a5a42d71bc065d
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-27 19:50:07 -0400

    Ultimo commit

--- 03-strings-numeros.py
+++ 03-strings-numeros.py
@@ -4,3 +4,32 @@ print ("Santiago\nHernandez\nGonzalez")
 print ("""Santiago
 Hernandez
 Gonzalez""")
+
+print ("Santiago", "Hernandez", "Gonzalez")
+
+# Números en Python
+
+# Números enteros
+print (30)
+
+# Números decimales
+print (30.5)
+
+# Operaciones
+print (3 + 2)
+print (3 - 2)
+print (3 * 2)
+print (3 / 2)
+
+# Módulo
+print (3 % 2)
+
+# Potencia
+print (3 ** 2)
+
+# Raíz cuadrada
+print (9 ** 0.5)
+
+# Orden de operaciones
+print (3 * (2 + 1)) # 9
+
