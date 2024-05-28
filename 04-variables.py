74bfca22282581e8b8fca9e855c08b78ac4baf21
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-27 20:52:59 -0400

    Ultimo commit

---
+++ 04-variables.py
@@ -0,0 +1,10 @@
+# variables
+
+# Variables are used to store data values.
+
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+nivel = input("Ingrese su nivel de estudios: ")
+
+print("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y mi nivel de estudios es " + nivel + ".")
+
