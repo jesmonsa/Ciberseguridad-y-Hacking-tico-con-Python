57ff43fd3a7814b03602e84f6deff487ffc16470
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-28 07:33:30 -0400

    Ultimo commit

---
+++ 05-comentarios.py
@@ -0,0 +1,27 @@
+# Esto es un comentario en Python
+# Los comentarios son líneas que no se ejecutan
+# Sirven para documentar el código
+# y hacerlo más legible
+# Los comentarios pueden ir al inicio de una línea
+# o al final de una línea de código
+
+# Comentario al inicio de una línea
+print("Hola Mundo") # Comentario al final de una línea de código 
+
+# Comentario de varias líneas
+# Esto es un comentario
+# de varias líneas
+# en Python
+# Se puede usar para explicar
+# un bloque de código
+# o para desactivar partes del código
+# print("Hola Mundo")
+
+nombre = input("Ingrese su nombre: ") # Solicitar al usuario su nombre
+edad = input("Ingrese su edad: ") # Solicitar al usuario su edad
+nivel = input("Ingrese su nivel de estudios: ") # Solicitar al usuario su nivel de estudios
+
+print("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y mi nivel de estudios es " + nivel + ".") # Imprimir mensaje con los datos ingresados por el usuario
+
+# Comentario de varias líneas
+# Variables
