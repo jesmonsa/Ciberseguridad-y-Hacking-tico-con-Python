701c70cbd594b65160c0a48d48447570950390e6
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-01 13:54:53 -0400

    Ultimo commit

--- 10-blucles-for-while.py
+++ 10-blucles-for-while.py
@@ -13,3 +13,32 @@ for i in range(len(lista)):
 
 for tarea in lista:
     print("Estoy trabajando en la tarea: ", tarea)
+
+lista_tareas = {
+    "tarea1": "Hacer la compra",
+    "tarea2": "Lavar el coche",
+    "tarea3": "Pasear al perro",
+    "tarea4": "Estudiar Python",
+    "tarea5": "Hacer deporte"
+}
+
+for tarea in lista_tareas:
+    print("Tarea: ", tarea)
+    print("Descripción: ", lista_tareas[tarea])
+
+
+# Bucle while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+num = 0
+while num < 10:
+    print(num)
+    num += 1
+
+while num > 0:
+    num -= 1
+    print(num)
+   
