a123d87670afb97d3b40135aa4c7775c4aa5c32e
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-01 12:48:12 -0400

    Ultimo commit

--- 09-if-else.py
+++ 09-if-else.py
@@ -1,6 +1,8 @@
 edad = int(input("Introduce tu edad: "))
-if edad >= 18:
-    print("Eres mayor de edad")
+if edad >= 18 and edad <= 21:
+    print("Eres mayor de edad en Esapaña pero no puedes beber alcohol en EEUU")
+elif edad >= 21:
+    print("Eres mayor de edad en España y en EEUU")
 else:
     print("Eres menor de edad")
 
