d2899f6021ab073d4a9560720e3bc4614c671ebf
Jesus Montoya <jesmonsa2018@gmail.com>
2024-05-31 20:29:03 -0400

    Ultimo commit

---
+++ 08-operadores.py
@@ -0,0 +1,25 @@
+# operadores
+
+numero_1 = 10
+numero_2 = 5
+
+string_1 = "Texto1"
+string_2 = "Texto2"
+
+lista_1 = ["valor1", "valor2", "valor3"]
+lista_2 = ["valor4", "valor5", "valor6"]
+
+tupla_1 = ("valor1", "valor2", "valor3")
+tupla_2 = ("valor4", "valor5", "valor6")
+
+diccionario_1 = {
+    "clave1": "valor1",
+    "clave2": "valor2",
+    "clave3": "valor3"
+}
+
+diccionario_2 = {
+    "clave4": "valor4",
+    "clave5": "valor5",
+    "clave6": "valor6"
+}
