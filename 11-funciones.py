f5f26b0ae552c3a9f0b25d0fffc45877cab17cb0
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-02 10:01:17 -0400

    Ultimo commit

--- 11-funciones.py
+++ 11-funciones.py
@@ -15,13 +15,34 @@ mi_funcion()
 def mi_funcion2(arg1, arg2):
     print(arg1 + arg2)
 
-mi_funcion2(1, 2)
+# función con parámetros posicionales
+
+def mi_funcion3(arg1, arg2):
+    print(arg1)
+    print(arg2)
+
+mi_funcion3(arg2="valor2", arg1="valor1")
 
 # Función con retorno
-def mi_funcion3(arg1, arg2):
+def mi_funcion4(arg1, arg2):
     return arg1 + arg2
 
-resultado = mi_funcion3(1, 2)
+resultado = mi_funcion4(1, 2)
 print(resultado)
+
+# Funciones con valores por defecto
+def mi_funcion5(arg1, arg2=100):
+    return arg1 + arg2
+
+resultado = mi_funcion5(1)
+print(resultado)
+
+# Funciones con retorno múltiple
+def mi_funcion6(arg1, arg2):
+    return arg1 + arg2, arg1 - arg2
+
+resultado = mi_funcion6(10, 5)
+print(resultado)
+
 
 
