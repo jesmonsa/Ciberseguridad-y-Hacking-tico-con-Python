300fc0c0b766c5729fe74866314f8b2856a7e5ec
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-02 05:50:05 -0400

    Ultimo commit

--- 11-funciones.py
+++ 11-funciones.py
@@ -3,3 +3,25 @@
 # Ejemlos de funciones en Python
 print("Esto es una cadena de texto")
 print(len("Esto es una cadena de texto"))
+
+# Definir una función
+def mi_funcion():
+    print("Hola desde mi función")
+
+# Llamar a la función
+mi_funcion()
+
+# Función con parámetros
+def mi_funcion2(arg1, arg2):
+    print(arg1 + arg2)
+
+mi_funcion2(1, 2)
+
+# Función con retorno
+def mi_funcion3(arg1, arg2):
+    return arg1 + arg2
+
+resultado = mi_funcion3(1, 2)
+print(resultado)
+
+
