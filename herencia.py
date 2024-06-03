4116f4d725674bc0e17f7d9a845947cc8a28fb90
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-03 10:54:17 -0400

    Ultimo commit

--- herencia.py
+++ herencia.py
@@ -21,9 +21,28 @@ class Coche():
               f"\nConsumo: {self.consumo} l/100km",
               "\nKilometros actuales:", self.km_actuales)
 
+    def actualizar_kilometros(self, kilometros):
+        """Actualiza los kilómetros actuales del coche.
+
+        Argumentos posicionales:
+        km -- int que representa los kilómetros recorridos
+        """
+        if kilometros > self.km_actuales:
+            self.km_actuales = kilometros 
+        else:
+            print("No se puede retroceder en el cuentakilómetros.")
+
+
 coche = Coche("Seat Ibiza", 90, 5)
 coche.especificaciones()
 coche1 = Coche("Mercedes Clase A", 150, 7)
 coche1.especificaciones()
 coche2 = Coche("Audi A3", 120, 6)
 coche2.especificaciones()
+
+# Asignamos valores a los atributos de instancia
+
+coche1.actualizar_kilometros(2000)
+coche1.actualizar_kilometros(1000)
+
+coche1.especificaciones()
