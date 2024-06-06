9688b702968977db1f36f2eb4c8dcbce24de319e
Jesus Montoya <jesmonsa2018@gmail.com>
2024-06-06 12:03:57 -0400

    Ultimo commit

--- 16-excepciones.py
+++ 16-excepciones.py
@@ -5,3 +5,19 @@ except:
     print("La variable no se ha definido previamente")
 
 print("El programa sigue ejecutandose")
+
+
+try:
+    print(var)
+
+except(NameError, ZeroDivisionError):
+    print("La variable no se ha definido previamente")
+
+print("El programa sigue ejecutandose")
+
+colores = ('azul', 'verde', 'amarillo')
+
+c = "morado"
+
+if c not in colores:
+    raise Exception(f"El color {c} no se encuentra entre los permitidos.")
